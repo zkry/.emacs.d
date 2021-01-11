27de4409827d1762d6447f5f76d756133cf7ca3e
Zachary Romero <zacromero@posteo.net>
2021-01-11 13:29:07 -0700

    add files

---
+++ personal/preload/preload.el
@@ -0,0 +1,24 @@
+(setq prelude-theme 'leuven)
+
+(add-to-list 'load-path "~/dev/emacs/org-mode/lisp/")
+(require 'org-loaddefs)
+(require 'org-duration)
+
+(add-to-list 'load-path "~/dev/emacs/org-mode/contrib/lisp/")
+
+(add-to-list 'load-path "~/dev/emacs/mu/mu4e")
+
+(add-to-list 'load-path "~/dev/emacs/awqat")
+
+(add-to-list 'load-path "~/dev/emacs/time-table")
+(require 'time-table)
+
+(add-to-list 'load-path "~/dev/emacs/org-roam")
+
+(add-to-list 'load-path "~/dev/emacs/org-fc/")
+(require 'org-fc)
+;(require 'org-fc-hydra)
+
+
+(add-to-list 'load-path "~/dev/emacs/browse-by-intention/")
+(require 'intentional)
